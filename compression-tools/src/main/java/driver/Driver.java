3eb80f599beb651a72918857630015055bf34bb1
ymakwan1 <ymakwan1@binghamton.edu>
2024-02-18 11:15:39 -0500

    Driver class updated'

--- compression-tools/src/main/java/driver/Driver.java
+++ compression-tools/src/main/java/driver/Driver.java
@@ -1,7 +1,15 @@
 package driver;
 
+import projectmanager.ProjectManager;
+
 public class Driver {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (args.length != 1) {
+            System.out.println("Usage : javac Driver.java filename.text or file path.");
+            System.exit(0);
+        }
+
+        ProjectManager projectManager = new ProjectManager(args[0]);
+        projectManager.runProject();
     }
 }
--- compression-tools/src/main/java/projectmanager/ProjectManager.java
+++ compression-tools/src/main/java/projectmanager/ProjectManager.java
@@ -1,4 +1,12 @@
 package projectmanager;
 
 public class ProjectManager {
+    String fileName;
+    public ProjectManager(String fileNameIn){
+        fileName = fileNameIn;
+    }
+
+    public void runProject(){
+
+    }
 }
